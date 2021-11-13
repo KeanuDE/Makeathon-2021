f86ca8e7797502aa068a710be38b1ccbc4695341
Keanu <skyloxkeanu@gmail.com>
2021-11-13 14:23:34 +0100

    .

--- api/app.js
+++ api/app.js
@@ -3,8 +3,10 @@ const cors = require('cors');
 const gpio = require('onoff').Gpio;
 
 const app = express();
-
 app.use(cors());
+
+var devices = [];
+
 
 app.get("/debug/:pin/:inout/:state",(req,res) => {
   var LED = new gpio(Number(req.params.pin),req.params.inout);
@@ -12,14 +14,27 @@ app.get("/debug/:pin/:inout/:state",(req,res) => {
   res.status(200);
 });
 
+
+
+
+
 //TODO: Geräte bekommen
 
 //TODO: Gerät hinzufügen
 
+//TODO: rgb code (lesen)
 
-//TODO: RGB CODE (lesen)
+//rgb code (schreiben)
+app.get("/sendRGB/:id/:r/:g/:b",(req,res) => {
+  var color = {
+    red: req.params.r,
+    green: req.params.g,
+    blue: req.params.b
+  }
+  //TODO: send code
+  res.status(200);
+})
 
-//TODO: RGB CODE (schreiben)
 
 
 
