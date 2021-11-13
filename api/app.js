7c48fc889546f9836ce117349a0e5eedc36b82c1
Keanu <skyloxkeanu@gmail.com>
2021-11-13 13:47:15 +0100

    debug command

--- api/app.js
+++ api/app.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(cors());
 
+app.get("/debug/:pin/:inout/:state",(req,res) => {
+  var LED = new gpio(Number(req.params.pin),req.params.inout);
+  LED.writeSync(Number(req.params.state));
+});
+
+
 app.listen(5565,() => {
   console.log("api server started.");
 });
