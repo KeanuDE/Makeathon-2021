204bed66946c2fd410cb11ffbe790a18539965e7
Keanu <skyloxkeanu@gmail.com>
2021-11-13 14:02:19 +0100

    Todos added

--- api/app.js
+++ api/app.js
@@ -9,7 +9,18 @@ app.use(cors());
 app.get("/debug/:pin/:inout/:state",(req,res) => {
   var LED = new gpio(Number(req.params.pin),req.params.inout);
   LED.writeSync(Number(req.params.state));
+  res.status(200).send({msg: "Keanu ist doof"});
 });
+
+//TODO: Geräte bekommen
+
+//TODO: Gerät hinzufügen
+
+
+//TODO: RGB CODE (lesen)
+
+//TODO: RGB CODE (schreiben)
+
 
 
 app.listen(5565,() => {
